901b3fa9c53a5c1ef0b9c7090712d317ce479bc8
Shrey Kavi <shrey_kavi@hotmail.com>
2021-05-31 19:45:44 -0400

    Init and learning println!
    Simply use cargo to init, build, run and learn how to print with
    RUST.
    
    Note it also includes how to import files and run functions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # learning_RUST
 Playground for learning RUST
+
+CLI:
+`rustc file.rc` to compile
+`cargo init` to init project
+`cargo run` to compile and run
+`cargo build` to compile
+`cargo build --release` to compile for prod
---
+++ src/print.rs
@@ -0,0 +1,28 @@
+pub fn run(){
+    // Print to console
+    println!("Hello from the print.rs file");
+
+    // Basic Formatting
+    println!("Number: {} for String: {}", 2, "test string");
+
+    // Positional Arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Shrey", "school", "code"
+    );
+
+    // Named Arguments
+    println!(
+        "{name} likes to play {activity}",
+        name = "Shrey",
+        activity = "Basketball"
+    );
+
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+    // Placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
+    
+    println!("10 + 10 = {}", 10+10);
+}
