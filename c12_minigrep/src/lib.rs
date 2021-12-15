dc737d459f20851515da03c3ec84d616972ebff2
Shrey Kavi <shrey.kavi@gmail.com>
2021-12-14 22:34:47 -0500

    Chapter 12: minigrep: TDD of search fn

--- c12_minigrep/src/lib.rs
+++ c12_minigrep/src/lib.rs
@@ -4,7 +4,9 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
@@ -33,4 +35,32 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    
+    results
 }
