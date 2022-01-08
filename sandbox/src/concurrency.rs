2a69a8647f71912cf510e5f093dddf15024f9052
Shrey Kavi <shrey.kavi@gmail.com>
2022-01-08 13:06:48 -0500

    Chapter 16: Message Channels
    “Do not communicate by sharing memory; instead, share memory by communicating.”

--- sandbox/src/concurrency.rs
+++ sandbox/src/concurrency.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn basic_thread_ex(){
     let v = vec![1, 2, 3];
@@ -30,6 +31,45 @@ fn basic_thread_ex(){
     handle.join().unwrap();
 }
 
+fn channel_comms(){
+    // multiple producer, single consumer :: channel
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // This would cause err since the channel now owns var
+        // println!("val is {}", val);
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    // .recv blocks and waits for comms, when tx closes this will error
+    // alternatively could use .try_recv() for non blocking
+    // let received = rx.recv().unwrap();
+    // println!("Got: {}", received);
+
+    // can also treat rx as a iterator for all messages in channel
+    // iteration ends once tx closes channel
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
 pub fn run(){
-    basic_thread_ex();
+    // basic_thread_ex();
+    channel_comms();
 }
