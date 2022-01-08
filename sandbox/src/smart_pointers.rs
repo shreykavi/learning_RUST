63304ba6505b0c4800e3535ce919fbcf24faa438
Shrey Kavi <shrey.kavi@gmail.com>
2022-01-08 11:10:29 -0500

    Chapter 15: RefCell and Interior Mut

--- sandbox/src/smart_pointers.rs
+++ sandbox/src/smart_pointers.rs
@@ -2,13 +2,16 @@
 // implements Deref and Drop
 
 use std::rc::Rc;
+use std::cell::RefCell;
 
 // Cons is construct fn which is used for recursive lists (not v useful in rust)
+#[derive(Debug)]
 enum List {
     // Using a box here allows rust to infer size correctly (since its now a pointer which is predictable)
     // Cons(i32, List),
     // Cons(i32, Box<List>),
-    Cons(i32, Rc<List>), // this was changed to Rc for multiple owners ex
+    // Cons(i32, Rc<List>), // this was changed to Rc for multiple owners ex
+    Cons(Rc<RefCell<i32>>, Rc<List>), // this change allows us to have both multiple owners and that you can mutate!
     Nil,
 }
 
@@ -79,30 +82,135 @@ fn box_ex(){
 }
 
 /// This shows an example of Rc counting the read only owners of the data
-fn multi_owner_ex(){
-    // Ex fails due to multiple owners
-    // let a = List::Cons(5, Box::new(List::Cons(10, Box::new(List::Nil))));
-    // let b = List::Cons(3, Box::new(a));
-    // let c = List::Cons(4, Box::new(a));
+// fn multi_owner_ex(){
+//     // Ex fails due to multiple owners
+//     // let a = List::Cons(5, Box::new(List::Cons(10, Box::new(List::Nil))));
+//     // let b = List::Cons(3, Box::new(a));
+//     // let c = List::Cons(4, Box::new(a));
 
-    // in this example we use Rc::clone rather than .clone() -> 
-    //          When looking for performance problems in the code,
-    //          we only need to consider the deep-copy clones and can 
-    //          disregard calls to Rc::clone
-    let a = Rc::new(List::Cons(5, Rc::new(List::Cons(10, Rc::new(List::Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = List::Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
+//     // in this example we use Rc::clone rather than .clone() -> 
+//     //          When looking for performance problems in the code,
+//     //          we only need to consider the deep-copy clones and can 
+//     //          disregard calls to Rc::clone
+//     let a = Rc::new(List::Cons(5, Rc::new(List::Cons(10, Rc::new(List::Nil)))));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let b = List::Cons(3, Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&a));
 
-    {
-        let c = List::Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+//     {
+//         let c = List::Cons(4, Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&a));
+//     }
+//     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// }
+
+pub fn interior_mut_ex(){
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(List::Cons(Rc::clone(&value), Rc::new(List::Nil)));
+
+    let b = List::Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = List::Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
-
 
 pub fn run(){
     // box_ex();
-    multi_owner_ex();
+    // multi_owner_ex();
+    interior_mut_ex();
+}
+
+
+// interior mutability in a test mocker example:
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+
+// In this test we use RefCell to allow us to borrow a mut ref after a immut ref
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+        // sent_messages: Vec<String>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+                // sent_messages: vec![],
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            // Since self is & (immutable) is cannot be borrowed as mutable here
+            // self.sent_messages.push(String::from(message));
+            
+            self.sent_messages.borrow_mut().push(String::from(message));
+
+            // Note this doesnt let us break the paradism of only being able to have 1
+            // mut ref at a time (Notice: RefCell panics on Runtime rather than compile)
+
+            // let mut one_borrow = self.sent_messages.borrow_mut();
+            // let mut two_borrow = self.sent_messages.borrow_mut();
+            // one_borrow.push(String::from(message));
+            // two_borrow.push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        // assert_eq!(mock_messenger.sent_messages.len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
 }
