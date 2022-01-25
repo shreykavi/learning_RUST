ccf202da9d544410599dd400cfeaab60de9f2c5e
Shrey Kavi <shreykavi@Shreys-MacBook-Pro.local>
2022-01-25 11:42:00 -0600

    Chapter 18: Pattern Syntax Examples

--- sandbox/src/patterns.rs
+++ sandbox/src/patterns.rs
@@ -71,10 +71,115 @@ fn refutable_ex(){
     // }
 }
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+fn diff_match_exs(){
+    // Multiple patterns "|"
+    let x = 1;
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // Range match "..="
+    let x = 5;
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    // Destructing
+    // 
+    // this extracts values from the underlying struct
+    let p = Point { x: 0, y: 7 };
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+    // shorthand:
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+    // matched destruction
+    let p = Point { x: 0, y: 7 };
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+    // works with nested enums too:
+    // let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+    // match msg {
+    //     Message::ChangeColor(Color::Rgb(r, g, b)) => println!(
+    //         "Change the color to red {}, green {}, and blue {}",
+    //         r, g, b
+    //     ),
+    //     Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+    //         "Change the color to hue {}, saturation {}, and value {}",
+    //         h, s, v
+    //     ),
+    //     _ => (),
+    // }
+
+    // Ignoring Remaining Parts ".." 
+    let origin = Point { x: 0, y: 0};
+    match origin {
+        Point { x, .. } => println!("x is {}", x),
+    }
+
+    // Conditional match
+    let x = Some(5);
+    let y = 10;
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+    println!("at the end: x = {:?}, y = {}", x, y);
+
+    // Capturing Range Binding "@"
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
+
 pub fn run(){
     conditional_ex();
     while_ex();
     for_ex();
     print_ex();
     refutable_ex();
+    diff_match_exs();
 }
